451f37a7bc02fc4f53bccbe3cfcedb4c92bc99cf
unknown <yairyarden@mail.tau.ac.il>
2025-07-27 18:08:24 +0300

    Create class SceneLoader that loads shapes and displays scene

---
+++ SceneLoader.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+import ShapeFactory
+
+
+class Scene:
+    def __init__(self):
+        self.shapes = []
+
+    def load_from_file(self, filename):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        for elem in root:
+            shape = ShapeFactory.create_shape(elem)
+            self.shapes.append(shape)
+
+    def render(self):
+        for shape in self.shapes:
+            shape.draw()

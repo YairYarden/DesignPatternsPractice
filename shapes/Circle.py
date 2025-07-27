bab758d1f460ab9f1517c2321f0b49742d849bae
unknown <yairyarden@mail.tau.ac.il>
2025-07-27 18:03:21 +0300

    add class Circle that inherits from Shape

---
+++ shapes/Circle.py
@@ -0,0 +1,23 @@
+import Shape
+
+
+class Circle(Shape):
+    def __init__(self, x, y, radius, fill_color, border_color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.fill_color = fill_color
+        self.border_color = border_color
+
+    @classmethod
+    def read_from_xml(cls, element):
+        return cls(
+            x=int(element.attrib['X']),
+            y=int(element.attrib['Y']),
+            radius=int(element.attrib['Radius']),
+            fill_color=element.attrib['FillingColor'],
+            border_color=element.attrib['Color']
+        )
+
+    def draw(self):
+        print(f"Drawing Circle at ({self.x}, {self.y}) with radius {self.radius}")

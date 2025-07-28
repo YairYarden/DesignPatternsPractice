4bfd75bcfb8d96b9582d890ecfd6a1d8e45a1f16
unknown <yairyarden@mail.tau.ac.il>
2025-07-28 18:10:47 +0300

    insert repeated code in scene_loader to function

--- scene_loader.py
+++ scene_loader.py
@@ -6,40 +6,34 @@ import cv2
 
 
 class Scene:
-    def __init__(self):
+    def __init__(self, filename):
         self.shapes = []
         self.shape_factory = ShapeFactory()
+        self.filename = filename
+        self.extension = filename.rsplit('.', 1)[-1] if '.' in filename else ''
 
-    def load_from_file(self, filename):
-        extension = filename.rsplit('.', 1)[-1] if '.' in filename else ''
-        if extension == "xml":
-            self.load_from_xml(filename)
-        elif extension == "json":
-            self.load_from_json(filename)
+    def load_from_file(self):
+        if self.extension == "xml":
+            self.load_from_xml(self.filename)
+        elif self.extension == "json":
+            self.load_from_json(self.filename)
         else:
-            raise ValueError(f"No support in file type : {extension}")
+            raise ValueError(f"No support in file type : {self.extension}")
 
     def load_from_xml(self, filename):
         tree = ET.parse(filename)
         root = tree.getroot()
-        for elem in root:
-            shape = self.shape_factory.create_shape_from_xml_elem(elem)
-            if shape is not None:
-                transforms = TransformFactory.create_transforms_from_xml_elem(elem)
-                for transform in transforms:
-                    shape.apply_transform(transform)
-                self.shapes.append(shape)
+        self.load_from_file_obj(root)
 
     def load_from_json(self, filename):
         with open(filename, 'r') as file:
             json_data = json.load(file)
+        self.load_from_file_obj(json_data)
 
-        for entry in json_data:
-            shape = self.shape_factory.create_shape_from_json_elem(entry)
+    def load_from_file_obj(self, file_obj):
+        for elem in file_obj:
+            shape = self.shape_factory.create_shape_from_element(elem, self.extension)
             if shape is not None:
-                transforms = TransformFactory.create_transforms_from_json_elem(entry)
-                for transform in transforms:
-                    shape.apply_transform(transform)
                 self.shapes.append(shape)
 
     def render(self, canvas):
--- shapes/shape_factory.py
+++ shapes/shape_factory.py
@@ -1,5 +1,6 @@
 from parsers.xml_shape_parser import XmlShapeParser
 from parsers.json_shape_parser import JsonShapeParser
+from transforms.transform_factory import TransformFactory
 
 
 class ShapeFactory:
@@ -9,8 +10,24 @@ class ShapeFactory:
         self.xml_parser = XmlShapeParser(self)
         self.json_parser = JsonShapeParser(self)
 
+    def create_shape_from_element(self, elem, file_type):
+        if file_type == 'xml':
+            return self.create_shape_from_xml_elem(elem)
+        elif file_type == 'json':
+            return self.create_shape_from_json_elem(elem)
+
     def create_shape_from_xml_elem(self, elem):
-        return self.xml_parser.parse(elem)
+        shape = self.xml_parser.parse(elem)
+        if shape is not None:
+            transforms = TransformFactory.create_transforms_from_xml_elem(elem)
+            for transform in transforms:
+                shape.apply_transform(transform)
+        return shape
 
     def create_shape_from_json_elem(self, elem):
-        return self.json_parser.parse(elem)
+        shape = self.json_parser.parse(elem)
+        if shape is not None:
+            transforms = TransformFactory.create_transforms_from_json_elem(elem)
+            for transform in transforms:
+                shape.apply_transform(transform)
+        return shape

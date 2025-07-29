f4c237ca9e77b879d0ad78012276a56fbac6152f
unknown <yairyarden@mail.tau.ac.il>
2025-07-29 10:28:29 +0300

    main function was written twice

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from scene_loader import Scene
 import numpy as np
 
 if __name__ == "__main__":
-    if __name__ == "__main__":
 
-        width, height = 400, 400
-        canvas = np.ones((height, width, 3), dtype=np.uint8) * 255  # white canvas
-
-        scene = Scene("step3_example.xml")
-        #scene = Scene("step4_example.json")
-        scene.load_from_file()
-        scene.render(canvas)
-        Scene.show_scene(canvas)
+    width, height = 400, 400
+    canvas = np.ones((height, width, 3), dtype=np.uint8) * 255  # white canvas
+    scene = Scene("step3_example.xml")
+    #scene = Scene("step4_example.json")
+    scene.load_from_file()
+    scene.render(canvas)
+    Scene.show_scene(canvas)
